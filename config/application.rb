77bd684374a1cef7a8459f6fa71c3201b04ce1f0
marsz <marsz330@gmail.com>
2017-06-27 02:26:41 +0800

    refine log

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,7 @@
 class App < Sinatra::Base
-  configure do
-    enable :logging
-    file = File.new("#{ROOT_DIR}/log/#{RACK_ENV}.log", 'a+')
-    file.sync = true
-    use Rack::CommonLogger, file
-  end
+
+  set :dump_errors, true
+  set :show_exceptions, true
 
   get '/' do
     TestService.new.a
