aa93c232c88d92b15f14cf46e1a91332e00a8395
marsz <marsz330@gmail.com>
2017-06-27 03:29:19 +0800

    show log

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ class App < Sinatra::Base
   end
 
   post '/deploy' do
+    puts "deploy##{params[:no]}"
     Deployer.new(params['deploy'].symbolize_keys).perform
+  end
+
+  get '/logging' do
+    res = `cat log/#{App.env}.log | awk 'BEGIN{ found=0} /deploy##{params[:no]}/{found=1}  {if (found) print }'`
+    res.to_s.gsub("\n", "<br />\n")
   end
 end
