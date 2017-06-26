d50706a7b1c5c855208d736aefd9fa92d9e66ab3
marsz <marsz330@gmail.com>
2017-06-27 03:34:11 +0800

    live reload param

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ class App < Sinatra::Base
   end
 
   get '/logging' do
+    reload_script = params['live'] ? '<script>setTimeout(function(){location.reload();}, 5000);</script>' : ''
     res = `cat log/#{App.env}.log | awk 'BEGIN{ found=0} /deploy##{params[:no]}/{found=1}  {if (found) print }'`
-    res.to_s.gsub("\n", "<br />\n")
+    res.to_s.gsub("\n", "<br />\n") + reload_script
   end
 end
